58637d0b4bf4176dbfe9c662fc3adb81a4c29edc
jacquesfu <jacques.fu@gmail.com>
2013-10-19 13:26:36 -0400

    Create nginxsetup.sh

---
+++ nginxsetup.sh
@@ -0,0 +1,2 @@
+# install common binaries
+yum install php php-mysql php-cli php-common php-pdo php-odbc php-pecl-apc php-pecl-memcache php-fpm php-soap mysql mysql-server mysql-libs mysql-server nginx
