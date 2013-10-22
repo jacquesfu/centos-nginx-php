9ea76069540a04780a21f015ea29539442b6503f
jacquesfu <jacques.fu@gmail.com>
2013-10-21 20:49:26 -0400

    Update nginxsetup.sh

--- nginxsetup.sh
+++ nginxsetup.sh
@@ -1,2 +1,2 @@
-# install common binaries
-yum install php php-mysql php-cli php-common php-pdo php-odbc php-pecl-apc php-pecl-memcache php-fpm php-soap mysql mysql-server mysql-libs mysql-server nginx emacs man
+# install common packages for php, mysql, nginx, man, emacs
+yum install -y php php-mysql php-cli php-common php-pdo php-odbc php-pecl-apc php-pecl-memcache php-fpm php-soap mysql mysql-server mysql-libs mysql-server nginx emacs man
